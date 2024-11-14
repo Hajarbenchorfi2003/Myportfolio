92f3d2c5cd174c3234441b77cfe0a74a72f1483b
Hajarbenchorfi2003 <benchorfihajar@gmail.com>
2024-11-14 22:29:42 +0100

    modfier projet

--- luiquereact-10/main/luique_react/src/utils/prismicClient.js
+++
@@ -1,4 +0,0 @@
-// utils/prismicClient.js
-import * as prismic from "@prismicio/client";
-
-export const client = prismic.createClient("https://portfoliohb.cdn.prismic.io/api/v2");
--- luiquereact-10/main/luique_react/src/utils/prismicHelpers.js
+++
@@ -1,6 +0,0 @@
-import { client } from "../prismic";
-
-export const fetchPrismicContent = async (documentType) => {
-  const response = await client.getByType(documentType);
-  return response?.results;
-};
